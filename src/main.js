109ecdae2fe2d486ee989b3bfd40c83895938b00
Mickael Riss <mendos.prod@gmail.com>
2024-01-29 13:08:54 -0500

    create app and install depedencies

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 
-createApp(App).mount('#app')
+library.add(faUserSecret)
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
