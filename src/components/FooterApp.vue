14644ceb8d081d0b9757d775a45243e42e538ec1
Mickael Riss <mendos.prod@gmail.com>
2024-01-29 19:29:31 -0500

    finish

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -1,7 +1,16 @@
 <template>
-  <footer class="footer m-auto mb-0 bg-black">
-    <p>Thanks a lot</p>
-  </footer>
+  <div class="footer bg-dark">
+    <div class="footer-links">
+      <a href="#"><i class="fab fa-github"></i></a>
+      <a href="#"><i class="fab fa-instagram"></i></a>
+      <a href="#"><i class="fab fa-facebook"></i></a>
+      <a href="#"><i class="fab fa-twitter"></i></a>
+      <a href="#"><i class="fab fa-linkedin"></i></a>
+    </div>
+    <div class="footer-copyright text-white">
+      This website is made with love by Mickael Riss
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,4 +20,29 @@ export default {
 </script>
 
 <style>
+  .footer {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 100px;
+    padding: 0px 50px;
+    color: rgba(0,0,0,0.3);
+  }
+  .footer-links {
+    display: flex;
+    align-items: center;
+  }
+  .footer-links a {
+    color: black;
+    opacity: 0.15;
+    text-decoration: none;
+    font-size: 24px;
+    padding: 0px 10px;
+  }
+  .footer-links a:hover {
+    opacity: 1;
+  }
+  .footer .fa-heart {
+    color: #D23333;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faUserSecret, faUser, faM, faBars } from '@fortawesome/free-solid-svg-icons'
+import { faUserSecret, faUser, faM, faBars, fas } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faUserSecret, faUser, faM, faBars)
+library.add(faUserSecret, faUser, faM, faBars, fas)
 
 createApp(App)
 .component('font-awesome-icon', FontAwesomeIcon)
